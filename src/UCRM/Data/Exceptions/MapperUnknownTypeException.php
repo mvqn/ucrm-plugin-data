d6ed1d9773cce005607aac9c87888302f642ebda
Ryan Spaeth <rspaeth@mvqn.net>
2018-08-03 10:03:25 -0700

    Added new Exception for MAPPER.

---
+++ src/UCRM/Data/Exceptions/MapperUnknownTypeException.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+
+namespace UCRM\Data\Exceptions;
+
+
+
+/**
+ * Class MapperUnknownTypeException
+ *
+ * @package UCRM\Data\Exceptions
+ * @author Ryan Spaeth <rspaeth@mvqn.net>
+ */
+final class MapperUnknownTypeException extends \Exception
+{
+}
